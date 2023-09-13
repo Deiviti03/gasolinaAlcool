e1397397487725f4262be9964cc7f2d633fda6e5
Deiviti Roque <deivitiroque1234@gmail.com>
2023-09-13 15:53:44 -0300

    initial-commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +7,32 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule]
 })
+
 export class HomePage {
+  
   constructor() {}
+
+  public resultado: String = "Resultado";
+  public precoAlcool: string = ""
+  public precoGasolina: string = ""
+
+  calcular(){
+    if( this.precoAlcool && this.precoGasolina ){
+
+      var pAlcool = parseFloat(this.precoAlcool)
+      var pGasolina = parseFloat(this.precoGasolina)
+
+     var res = pAlcool / pGasolina
+
+     if(res >= 0.7){
+      this.resultado = "Melhor usar Gasolina!"
+     }else{
+      this.resultado = "Melhor usar Álcool!"
+     }
+    }else{
+      this.resultado = "Preencha corretamente os campos!"
+    }
+  }
 }
